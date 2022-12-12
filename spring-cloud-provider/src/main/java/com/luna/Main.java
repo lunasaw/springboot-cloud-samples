2dc29cbca250694c189005b9f11ed5fe7e009029
chenzhangyue <chenzhangyue01@weidian.com>
2022-12-12 10:59:08 +0800

    spring cloud alibaba in 2021.0.1.0 not support spring-cloud-dubbo component

---
+++ spring-cloud-provider/src/main/java/com/luna/Main.java
@@ -0,0 +1,11 @@
+package com.luna;
+
+/**
+ * @author chenzhangyue
+ * ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
